ec5130fad513ae8588cd411f50ac522a82707ad7
ElijahA1 <elijah.abourezk@outlook.com>
2025-07-22 16:08:15 -0700

    Add project files.

---
+++ Trump It!/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Trump_It_
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void homeCardTapped(object sender, TappedEventArgs e)
+        {
+            if (sender is Image tappedImage)
+            {
+                await tappedImage.ScaleTo(1.2, 150, Easing.CubicOut);
+                await tappedImage.ScaleTo(1.0, 150, Easing.CubicIn);
+            }
+        }
+    }
+}
---
+++ Trump It!/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Trump_It_
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
