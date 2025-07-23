2ccf5c2ab85689e55d8504cba63616f5530a6a7b
ElijahA1 <elijah.abourezk@outlook.com>
2025-07-22 21:13:08 -0700

    Trumpet Animation complete and added

--- Trump It!/MainPage.xaml.cs
+++ Trump It!/MainPage.xaml.cs
@@ -1,19 +1,28 @@
-﻿namespace Trump_It_
+﻿using System;
+using System.Threading.Tasks;
+using SkiaSharp.Extended.UI.Controls;    // for SKLottieView
+using Microsoft.Maui.Controls;
+
+namespace Trump_It_
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
+
         private async void homeCardTapped(object sender, TappedEventArgs e)
         {
             if (sender is Image tappedImage)
             {
                 await tappedImage.ScaleTo(1.2, 150, Easing.CubicOut);
                 await tappedImage.ScaleTo(1.0, 150, Easing.CubicIn);
+
+                trumpetAnim.IsAnimationEnabled = false;    // stop current run
+                trumpetAnim.Progress = TimeSpan.Zero;      // rewind to frame 0
+                await Task.Delay(20);                      // allow control to update
+                trumpetAnim.IsAnimationEnabled = true;     // start playing
             }
         }
     }
